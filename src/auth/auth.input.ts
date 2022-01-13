6ff0121846073961a5d5163d56c53623730d62be
Eduardo Reyes <ereyesoyarzo@gmail.com>
2022-01-13 12:08:38 -0300

    add validation for schema

--- src/auth/auth.input.ts
+++ src/auth/auth.input.ts
@@ -1,22 +1,30 @@
 import { Field, InputType } from '@nestjs/graphql'
+import { IsEmail, IsNotEmpty } from 'class-validator'
 
 @InputType()
 export class SignUpInput {
     @Field()
+    @IsNotEmpty()
     name: string
 
     @Field()
+    @IsEmail()
+    @IsNotEmpty()
     email: string
 
     @Field()
+    @IsNotEmpty()
     password: string
 }
 
 @InputType()
 export class LoginInput {
     @Field()
+    @IsEmail()
+    @IsNotEmpty()
     email: string
 
     @Field()
+    @IsNotEmpty()
     password: string
 }
